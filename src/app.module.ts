edbcc862a00b9b604809983d0c8931e65dac4df4
medo <medohasan124@gmail.com>
2025-09-09 13:25:08 +0300

    create Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { PostsController } from './posts/posts.controller';
-import { PostsService } from './posts/posts.service';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController, PostsController],
-  providers: [AppService, UsersService, PostsService],
+  imports: [UsersModule, PostsModule],
+  controllers: [AppController ],
+  providers: [AppService] ,
 })
 export class AppModule {}
